a392554f9f4e129667d86291f077cf9684b97392
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-04-26 18:27:01 +0530

    Add RestClient timeout settings

--- modern-spring-boot/src/main/java/com/sivalabs/modernboot/restclient/RestClientConfig.java
+++ modern-spring-boot/src/main/java/com/sivalabs/modernboot/restclient/RestClientConfig.java
@@ -1,18 +1,32 @@
 package com.sivalabs.modernboot.restclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 class RestClientConfig {
 
     @Bean
     RestClient restClient(@Value("${placeholder_api_base_uri}") String baseURI) {
+        ClientHttpRequestFactory requestFactory =
+                ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withReadTimeout(Duration.ofSeconds(5)));
         return RestClient
                 .builder()
                 .baseUrl(baseURI)
+                .requestFactory(requestFactory)
+                .defaultStatusHandler(HttpStatusCode::isError, (req, response) -> {
+                    throw new RuntimeException("Error occurred while calling API: " + response.getStatusCode());
+                })
                 .build();
     }
 }
